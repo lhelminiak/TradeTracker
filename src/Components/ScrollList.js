3b20df7fdde9aff36da54d83cbe6d8b2844e362a
Drake <drakeeman@yahoo.com>
2018-02-13 14:36:14 -0600

    Pull to Refresh Added

--- src/Components/ScrollList.js
+++ src/Components/ScrollList.js
@@ -24,16 +24,16 @@ export default class ScrollList extends React.Component {
                         {title: 'Open Trades', data: ['Hey Look']},
                         {title: 'Closed Trades', data: ['Alyssa', 'Brooke', 'Rachel', 'Margaret', 'Mia?', 'Some girl named Carley somewhere', 'Gabby Pinto',
                             'Morgan cant forget her too', 'Drake cuz your gay', 'Never Marta tho thats weird',
-                            'Alexa Ostrega', 'Sorry I typed all this it was just to see the Section List in action' ]},
+                            'Alexa Ostrega', 'Sorry I typed all this love you' ]},
+                    ]}
 
-
-                        ]}
-
-
+                    refreshing={true}
+                    onRefresh={() => this.onRefresh()}
+                    showsVerticalScrollIndicator={false}
                     ItemSeparatorComponent={({separator}) =>
                         <View style={styles.separator}>{separator}
-                            </View>}
-                        renderItem={({item}) =>
+                        </View>}
+                    renderItem={({item}) =>
                         <Text style={styles.item}>{item}
                         </Text>}
                     renderSectionHeader={({section}) =>
@@ -49,30 +49,30 @@ export default class ScrollList extends React.Component {
     }
 }
 
-                const styles = StyleSheet.create({
-                container: {
-                flex: 1,
-            },
-                sectionHeader: {
-                paddingTop: 4,
-                paddingLeft: 15,
-                paddingRight: 15,
-                paddingBottom: 4,
-                fontSize: 16,
-                fontWeight: '700',
-                color: '#FFFFFF',
-                backgroundColor: '#34a8ff',
-            },
-                separator: {
-                height: 1,
-                backgroundColor: '#65c3cc'
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    sectionHeader: {
+        paddingTop: 4,
+        paddingLeft: 15,
+        paddingRight: 15,
+        paddingBottom: 4,
+        fontSize: 16,
+        fontWeight: '700',
+        color: '#FFFFFF',
+        backgroundColor: '#34a8ff',
+    },
+    separator: {
+        height: 1,
+        backgroundColor: '#65c3cc'
 
-            },
+    },
 
 
-                item: {
-                padding: 10,
-                fontSize: 18,
-                height: 44,
-            },
-            })
+    item: {
+        padding: 10,
+        fontSize: 18,
+        height: 44,
+    },
+})
